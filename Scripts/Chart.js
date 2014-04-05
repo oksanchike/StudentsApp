fc923dbb54b4fda29f1a62104bf0298e42caa93a
oksanchike <oksanchike@gmail.com>
2014-04-05 22:45:58 +0600

    Убрала двойное рисование бублика

--- Scripts/Chart.js
+++ Scripts/Chart.js
@@ -9,14 +9,12 @@
 
     },
     drawForStudent: function (dataStudentsPresence, dataSubjects) {
-        this.dataStudentsPresence = dataStudentsPresence;
-        this.dataSubjects = dataSubjects;
+        this.__dataStudentsPresence = dataStudentsPresence;
+        this.__dataSubjects = dataSubjects;
 
         var totalHours = this.getTotalLength();
         this.drawLineChart(totalHours);
-
-        this.drawSlices(true);
-        this.drawSlices(false);
+        this.drawDoughnutChart();
         this.drawText();
         //this.drawDivisionLabel();
     },
@@ -44,8 +42,8 @@
         context.save();
         var height = 25;
         var colors = ["#9999FF", "#CC6699", "#3366FF", "#33CC99", "#CC6633", "#FFCCFF", "#FFFF99", "#CCFFFF"];
-        for (var i = 0; i < this.dataSubjects.length; i++) {
-            var rectangleWidth = this.dataSubjects[i].totalTime / ratio;
+        for (var i = 0; i < this.__dataSubjects.length; i++) {
+            var rectangleWidth = this.__dataSubjects[i].totalTime / ratio;
             context.beginPath();
             context.rect(x, y, rectangleWidth, height);
             context.fillStyle = colors[i];
@@ -75,31 +73,21 @@
         }
         context.restore();
     },
-    drawRectangleSmall: function (color, x, y) {
-        var context = this.context;
-        context.save();
-        context.beginPath();
-        context.rect(x, y, 10, 10);
-        context.fillStyle = color;
-        context.fill();
-        context.closePath();
-        context.restore();
-    },
     drawText: function () {
         var context = this.context;
         context.save();
         context.fillStyle = "black";
         context.beginPath();
         context.font = 'normal 13pt PT Sans';
-        context.fillText(this.dataSubjects[0].title + " ( " + this.dataSubjects[0].totalTime + " часов )", 20, 80);
+        context.fillText(this.__dataSubjects[0].title + " ( " + this.__dataSubjects[0].totalTime + " часов )", 20, 80);
 
         context.fillStyle = "black";
         context.font = 'normal 11pt PT Sans';
         context.fillText("Всего пройдено", this.pieX - 50, this.pieY - 50);
         context.font = 'bold 11pt PT Sans';
         context.fillStyle = "#FFCC00";
-        var px = this.context.measureText(this.dataSubjects[0].elapsedTime).width / 2;
-        context.fillText(this.dataSubjects[0].elapsedTime, this.pieX - px, this.pieY - 30);
+        var px = this.context.measureText(this.__dataSubjects[0].elapsedTime).width / 2;
+        context.fillText(this.__dataSubjects[0].elapsedTime, this.pieX - px, this.pieY - 30);
         //this.drawRectangleSmall("#FFCC00", this.pieX - 25, this.pieY - 40);
 
         context.fillStyle = "black";
@@ -107,8 +95,8 @@
         context.fillText("Всего прогуляно", this.pieX - 44, this.pieY - 10);
         context.font = 'bold 11pt PT Sans';
         context.fillStyle = "rgb(153, 0, 0)";
-        px = this.context.measureText(this.dataStudentsPresence[0].totalAbsenseTime).width / 2;
-        context.fillText(this.dataStudentsPresence[0].totalAbsenseTime, this.pieX - px, this.pieY + 10);
+        px = this.context.measureText(this.__dataStudentsPresence[0].totalAbsenseTime).width / 2;
+        context.fillText(this.__dataStudentsPresence[0].totalAbsenseTime, this.pieX - px, this.pieY + 10);
         //this.drawRectangleSmall("rgb(153, 0, 0)", this.pieX - 25, this.pieY);
 
         context.fillStyle = "black";
@@ -117,8 +105,8 @@
         context.fillText("по уважительной причине", this.pieX - 63, this.pieY + 45);
         context.font = 'bold 11pt PT Sans';
         context.fillStyle = "#0099CC";
-        px = this.context.measureText(this.dataStudentsPresence[0].withValidReasonTime).width / 2;
-        context.fillText(this.dataStudentsPresence[0].withValidReasonTime, this.pieX - px, this.pieY + 65);
+        px = this.context.measureText(this.__dataStudentsPresence[0].withValidReasonTime).width / 2;
+        context.fillText(this.__dataStudentsPresence[0].withValidReasonTime, this.pieX - px, this.pieY + 65);
         //this.drawRectangleSmall("#0099CC", this.pieX - 25, this.pieY + 55);
 
         context.fillStyle = "black";
@@ -127,36 +115,46 @@
 
         context.fillStyle = "black";
         context.font = 'normal 11pt PT Sans';
-        px = this.context.measureText(this.dataSubjects[0].totalTime / 2).width / 2;
-        context.fillText(this.dataSubjects[0].totalTime / 2, this.pieX - px, this.pieY + 145);
+        px = this.context.measureText(this.__dataSubjects[0].totalTime / 2).width / 2;
+        context.fillText(this.__dataSubjects[0].totalTime / 2, this.pieX - px, this.pieY + 145);
 
         context.fillStyle = "black";
         context.font = 'normal 11pt PT Sans';
-        context.fillText(this.dataSubjects[0].totalTime / 4, this.pieX + 135, this.pieY);
+        context.fillText(this.__dataSubjects[0].totalTime / 4, this.pieX + 135, this.pieY);
 
         context.fillStyle = "black";
         context.font = 'normal 11pt PT Sans';
-        px = this.context.measureText(this.dataSubjects[0].totalTime / 4 + this.dataSubjects[0].totalTime / 2).width;
-        context.fillText(this.dataSubjects[0].totalTime / 4 + this.dataSubjects[0].totalTime / 2, this.pieX - 135 - px, this.pieY);
+        px = this.context.measureText(this.__dataSubjects[0].totalTime / 4 + this.__dataSubjects[0].totalTime / 2).width;
+        context.fillText(this.__dataSubjects[0].totalTime / 4 + this.__dataSubjects[0].totalTime / 2, this.pieX - 135 - px, this.pieY);
 
         context.closePath();
         context.restore();
     },
-    drawSlices: function (withShadow) {
+    drawEmptyDoughnut: function () {
         var context = this.context;
         context.save();
-        if (withShadow) {
-            context.shadowBlur = 15;
-            context.shadowColor = "#999";
-            context.shadowOffsetX = 0;
-            context.shadowOffsetY = 3;
-        }
-        var slice = this.dataStudentsPresence[0];
-        var sliceSubject = this.dataSubjects[0];
+        context.shadowBlur = 15;
+        context.shadowColor = "#999";
+        context.shadowOffsetX = 0;
+        context.shadowOffsetY = 3;
+        context.beginPath();
+        context.arc(this.pieX, this.pieY, this.pieRadius - 30, 0, 2*Math.PI, false);
+        context.lineWidth = 30;
+        context.strokeStyle = "#E8E8E8";
+        context.stroke();
+        context.closePath();
+        context.restore();
+    },
+    drawDoughnutChart: function () {
+        this.drawEmptyDoughnut();
+        var context = this.context;
+        context.save();
+        var slice = this.__dataStudentsPresence[0];
+        var sliceSubject = this.__dataSubjects[0];
         var total = sliceSubject.totalTime;
         var startAngle = 1.5 * Math.PI;
         var color = "#0099CC";
-        for (var i = 0; i < 4; i++) {
+        for (var i = 0; i < 3; i++) {
             if (i == 0) {
                 var sliceAngle = 2 * Math.PI * slice.withValidReasonTime / total; //нужно пересчитать
             }
@@ -167,10 +165,6 @@
             if (i == 2) {
                 var sliceAngle = 2 * Math.PI * (sliceSubject.elapsedTime - slice.totalAbsenseTime) / total; //нужно пересчитать
                 color = "#FFCC00";
-            }
-            if (i == 3) {
-                var sliceAngle = 2 * Math.PI * (total - sliceSubject.elapsedTime) / total; //нужно пересчитать
-                color = "#E8E8E8";
             }
             var endAngle = startAngle + sliceAngle;
 
@@ -185,7 +179,7 @@
         context.restore();
     },
     getTotalLength: function () {
-        var dataSubjects = this.dataSubjects;
+        var dataSubjects = this.__dataSubjects;
         var total = 0;
 
         for (var i = 0; i < dataSubjects.length; i++) {
